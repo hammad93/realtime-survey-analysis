595d2491958c592c13d84cecd9933647b88e37f1
cwg930 <cwg930@gmail.com>
2015-05-29 02:52:20 -0400

    Added a list of items and title bar.

--- Android/UCFSurveys/app/src/main/java/com/ucfsurveys/ucfsurveys/SurveySelection.java
+++ Android/UCFSurveys/app/src/main/java/com/ucfsurveys/ucfsurveys/SurveySelection.java
@@ -2,37 +2,42 @@ package com.ucfsurveys.ucfsurveys;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class SurveySelection extends Activity {
-
+public class SurveySelection extends Activity implements View.OnClickListener {
+    int i = 0;
+    TextView textView;
+    ListView SurveyList;
+    ArrayAdapter mArrayAdapter;
+    ArrayList<String> mNameList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_selection);
-    }
+        textView = (TextView)findViewById(R.id.survey_available_text);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_survey_selection, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        SurveyList = (ListView)findViewById(R.id.Survey_Listview);
+        for(; i < 10; i++){
+            mNameList.add(getString(R.string.survey_name) + i);
         }
+        mArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, mNameList);
 
-        return super.onOptionsItemSelected(item);
+        SurveyList.setAdapter(mArrayAdapter);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        i++;
+        mNameList.add(getString(R.string.survey_name) + i);
+        mArrayAdapter.notifyDataSetChanged();
     }
 }
