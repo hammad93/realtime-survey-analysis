b5be7d69a1982a6e06a532edd47ad8b6f902d3d4
cwg930 <cwg930@gmail.com>
2015-06-14 15:43:08 -0400

    added webapp code
    webapp added

---
+++ WebAppSurvey/WebApplication4/Models/Survey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; 
+
+
+namespace WebApplication4.Models
+{
+    public class Survey
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime  DateCreated { get; set; }  
+
+    }
+
+  
+
+    public class MovieDBContext: ApplicationDbContext{
+
+        public DbSet<Survey> Survey {get; set; }
+
+    }
+}
