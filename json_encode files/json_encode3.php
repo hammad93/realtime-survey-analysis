e826013356c878a7b2d12327960fb0c174221540
hammad93 <ham.u@live.com>
2015-06-21 19:04:35 -0400

    json_encode

---
+++ json_encode files/json_encode3.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password)
+*/
+$dbhost = '50.62.209.77:3306';
+$dbuser = 'database_admin';
+$dbpass = '1hnjNF6m!';
+$dbname = 'ucfsurveys';
+$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$result = mysqli_query($link,"SELECT * FROM `SURVEY_3` WHERE 1");
+
+while($row = mysqli_fetch_array($result)){
+	echo json_encode($row);
+}
+ 
+?>
