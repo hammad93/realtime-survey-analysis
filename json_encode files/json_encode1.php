90c8edabf2e26cb405d08e20543a17865c231c7c
cwg930 <cwg930@gmail.com>
2015-06-24 19:01:02 -0400

    changed it to make getting question choices easier

--- json_encode files/json_encode1.php
+++ json_encode files/json_encode1.php
@@ -14,10 +14,27 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$result = mysqli_query($link,"SELECT * FROM `SURVEY_1` WHERE 1");
+$result = mysqli_query($link,"SELECT * FROM `SURV1EY_1` WHERE 1");
+$choices = "SELECT answer_num, text FROM SURVEY_1_CHOICES WHERE question_id=?";
 $response = array();
 while($row = mysqli_fetch_assoc($result)){
+	if($row['num_choices'] > 0){
+		$stmt = mysqli_stmt_init($link);
+		if(mysqli_stmt_prepare($stmt, $choices)){
+			mysqli_stmt_bind_param($stmt, "i",$row['question_id']);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_bind_result($stmt, $num, $text);
+			$row['choices'] = array();
+			$i = 0;
+			while(mysqli_stmt_fetch($stmt)){
+				$row['choices'][i]['num'] = $num;
+				$row['choices'][i]['text'] = $text;
+				$i = $i + 1;
+			}
+		}
+	}
 	$response[] = $row;
+
 }
  echo json_encode($response);
  
